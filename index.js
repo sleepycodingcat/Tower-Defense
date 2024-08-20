70afe69dd337481ac7ad469ece5ab1ec3245614f
Rachell Aristo <123459774+sleepycodingcat@users.noreply.github.com>
2024-08-20 20:36:55 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+import {
+  Project,
+  Sprite,
+} from "https://unpkg.com/leopard@^1/dist/index.esm.js";
+
+import Stage from "./Stage/Stage.js";
+import Enemy from "./Enemy/Enemy.js";
+import Line from "./Line/Line.js";
+
+const stage = new Stage({ costumeNumber: 1 });
+
+const sprites = {
+  Enemy: new Enemy({
+    x: 240,
+    y: -180,
+    direction: 114,
+    rotationStyle: Sprite.RotationStyle.ALL_AROUND,
+    costumeNumber: 3,
+    size: 60,
+    visible: false,
+    layerOrder: 2,
+  }),
+  Line: new Line({
+    x: 0,
+    y: 0,
+    direction: 90,
+    rotationStyle: Sprite.RotationStyle.ALL_AROUND,
+    costumeNumber: 1,
+    size: 100,
+    visible: true,
+    layerOrder: 1,
+  }),
+};
+
+const project = new Project(stage, sprites, {
+  frameRate: 30, // Set to 60 to make your project run faster
+});
+export default project;
